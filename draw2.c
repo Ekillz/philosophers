0dbc7b9bde68c4baf55583a94249dfb1127e74fb
Emil MAMMADZADA <emammadz@e1r9p16.42.fr>
2015-10-13 18:35:27 +0200

    added more detail to graphic interface

--- draw2.c
+++ draw2.c
@@ -6,37 +6,16 @@
 /*   By: emammadz <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/10/13 15:39:49 by emammadz          #+#    #+#             */
-/*   Updated: 2015/10/13 17:22:44 by emammadz         ###   ########.fr       */
+/*   Updated: 2015/10/13 18:34:58 by emammadz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.h"
 
-static int		get_philo_coord(t_env *e, int which, char coord)
-{
-	int i;
-	int res;
-
-	i = 0;
-	while (i < 7)
-	{
-		if (e[i].nb == which)
-		{
-			if (coord == 'x')
-				res = e[i].x;
-			else
-				res = e[i].y;
-			return (res);
-		}
-		i++;
-	}
-	return (-1);
-}
-
 static char		*get_char(int which)
 {
 	char *new;
-	
+
 	if (which == -1)
 		new = ft_itoa(MAX_LIFE);
 	else if (which == -2)
@@ -46,10 +25,30 @@ static char		*get_char(int which)
 	else if (which == -4)
 		new = ft_itoa(THINK_T);
 	else if (which == -5)
-		new = ft_itoa(TIMEOUT);	
+		new = ft_itoa(TIMEOUT);
 	else
 		new = ft_itoa(which);
 	return (new);
+}
+
+static void		show_life(t_graph *t, char **ptr)
+{
+	int i;
+
+	i = 0;
+	while (i < 7)
+	{
+		write(1, "", 0);
+		mlx_string_put(t->mlx, t->win,
+				t->e[i].x, t->e[i].y, 0xffff00,
+				ptr[i + 5] = get_char(t->e[i].life));
+		mlx_string_put(t->mlx, t->win,
+				t->e[i].x, t->e[i].y - 35, 0xffff00,
+				ptr[i + 12] = get_char(t->e[i].nb));
+		i++;
+	}
+	ptr[19] = NULL;
+	ft_freetab(ptr);
 }
 
 void			show_header_info(t_graph *t)
@@ -57,10 +56,10 @@ void			show_header_info(t_graph *t)
 	int		i;
 	char	*t_ime;
 	char	**ptr;
-		
+
 	t_ime = ft_itoa(time(0) - t->time);
 	i = 0;
-	ptr = malloc(sizeof(char *) * 13);
+	ptr = malloc(sizeof(char *) * 20);
 	mlx_string_put(t->mlx, t->win, 40, 50, 0xffff00, "EAT");
 	mlx_string_put(t->mlx, t->win, 140, 50, 0xffff00, "REST");
 	mlx_string_put(t->mlx, t->win, 240, 50, 0xffff00, "THINK");
@@ -75,13 +74,6 @@ void			show_header_info(t_graph *t)
 	mlx_string_put(t->mlx, t->win, 500, 80, 0xffff00, ptr[3] = get_char(-4));
 	mlx_string_put(t->mlx, t->win, 400, 100, 0xffff00, "TIMEOUT: ");
 	mlx_string_put(t->mlx, t->win, 500, 100, 0xffff00, ptr[4] = get_char(-5));
-	while (i < 7)
-	{
-		mlx_string_put(t->mlx, t->win,
-				get_philo_coord(t->e, i, 'x'), get_philo_coord(t->e, i, 'y'), 0xffff00, ptr[i + 5] = get_char(t->e[i].life));
-		i++;
-	}
+	show_life(t, ptr);
 	free(t_ime);
-	ptr[12] = NULL;
-	ft_freetab(ptr);
 }
--- ft_memdel.c
+++ ft_memdel.c
@@ -6,13 +6,13 @@
 /*   By: emammadz <emammadz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/04 18:18:08 by emammadz          #+#    #+#             */
-/*   Updated: 2015/10/13 16:41:24 by emammadz         ###   ########.fr       */
+/*   Updated: 2015/10/13 18:27:02 by emammadz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.h"
 
-void ft_memdel(void **ap)
+void	ft_memdel(void **ap)
 {
 	if (ap != NULL)
 	{
